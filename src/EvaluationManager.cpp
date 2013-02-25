fff2c076faf808e23d421c554d37611014adca1f
Yasuharu Sawada <yasuharu519@gmail.com>
2013-02-25 18:38:22 +0900

    EvaluationManagerひな形の追加

---
+++ src/EvaluationManager.cpp
@@ -0,0 +1,12 @@
+#include "EvaluationManager.hpp"
+
+// Constructor/Destructor
+EvaluationManager::EvaluationManager()
+{
+    // 初期化
+}
+
+EvaluationManager::~EvaluationManager()
+{
+    // 後始末
+}
---
+++ src/EvaluationManager.hpp
@@ -0,0 +1,14 @@
+#ifndef __EVALUATION_MANAGER_HPP__
+#define __EVALUATION_MANAGER_HPP__
+
+using namespace std;
+
+
+class EvaluationManager{
+    public:
+        // Constructor/Destructor
+        EvaluationManager();
+        virtual ~EvaluationManager();
+};
+
+#endif

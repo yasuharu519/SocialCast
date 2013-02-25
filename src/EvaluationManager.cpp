99f80f163e7e67a47c056595f0f84f155bda4d9f
Yasuharu Sawada <yasuharu519@gmail.com>
2013-02-25 19:05:26 +0900

    EvaluationManagerの簡単な関数作成

--- src/EvaluationManager.cpp
+++ src/EvaluationManager.cpp
@@ -3,10 +3,44 @@
 // Constructor/Destructor
 EvaluationManager::EvaluationManager()
 {
-    // 初期化
+    clearEvaluationData();
 }
 
 EvaluationManager::~EvaluationManager()
 {
     // 後始末
 }
+
+//Public
+void EvaluationManager::clearEvaluationData()/*{{{*/
+{
+    numPathHopCount = 0;
+    trialTimes = 0;
+    numPackets = 0;
+    distributeLatency = 0.0;
+    penalty = 0.0;
+    timePacketInQueue = 0.0;
+    timeContentGet = 0.0;
+}/*}}}*/
+
+void EvaluationManager::addTimePacketInQueue(double _inQueueTime)/*{{{*/
+{
+    ++numPackets;
+    timePacketInQueue += _inQueueTime;
+}/*}}}*/
+
+double EvaluationManager::getAverageTimePacketInQueue()/*{{{*/
+{
+    return timePacketInQueue / numPackets;
+}/*}}}*/
+
+void EvaluationManager::addTimeContentGet(double _contentGetTime)/*{{{*/
+{
+    ++numContents;
+    timeContentGet += _contentGetTime;
+}/*}}}*/
+
+double EvaluationManager::getAverageTimeContentGet()/*{{{*/
+{
+    return timeContentGet / numContents;
+}/*}}}*/
--- src/EvaluationManager.hpp
+++ src/EvaluationManager.hpp
@@ -9,6 +9,22 @@ class EvaluationManager{
         // Constructor/Destructor
         EvaluationManager();
         virtual ~EvaluationManager();
+        void clearEvaluationData();
+        // PacketInQueue
+        void addTimePacketInQueue(double _inQueueTime);
+        double getAverageTimePacketInQueue();
+        // ContentGetTime
+        void addTimeContentGet(double _contentGetTime);
+        double getAverageTimeContentGet();
+    private:
+        long trialTimes;
+        long numPathHopCount;
+        long numPackets;
+        long numContents;
+        double distributeLatency;
+        double penalty;
+        double timePacketInQueue;
+        double timeContentGet;
 };
 
 #endif

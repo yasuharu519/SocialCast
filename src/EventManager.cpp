062cd393017917a95374015239b0af9f65f27875
Yasuharu Sawada <yasuharu519@gmail.com>
2013-02-25 18:20:41 +0900

    EventManagerの実装

--- src/EventManager.cpp
+++ src/EventManager.cpp
@@ -4,62 +4,104 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Event
 ///////////////////////////////////////////////////////////////////////////////
-Event::Event(){
-
+Event::Event(double _time){
+    time = _time;
 }
 
 Event::~Event(){
 
 }
 
+//Public
+double Event::getEventTime() const
+{
+    return time;
+}
 
 // ------------------------------------------------------------------------- //
 ///////////////////////////////////////////////////////////////////////////////
 // ContentRequestedEvent
 ///////////////////////////////////////////////////////////////////////////////
-ContentRequestedEvent::ContentRequestedEvent(){
+//ContentRequestedEvent::ContentRequestedEvent(){
 
-}
+//}
 
-ContentRequestedEvent::~ContentRequestedEvent(){
+//ContentRequestedEvent::~ContentRequestedEvent(){
 
-}
+//}
 
 
 // ------------------------------------------------------------------------- //
 ///////////////////////////////////////////////////////////////////////////////
 // SendPacketEvent
 ///////////////////////////////////////////////////////////////////////////////
-SendPacketEvent::SendPacketEvent(){
+//SendPacketEvent::SendPacketEvent(){
 
-}
+//}
 
-SendPacketEvent::~SendPacketEvent(){
+//SendPacketEvent::~SendPacketEvent(){
 
-}
+//}
 
 
 // ------------------------------------------------------------------------- //
 ///////////////////////////////////////////////////////////////////////////////
 // ReceivePacketEvent
 ///////////////////////////////////////////////////////////////////////////////
-ReceivePacketEvent::ReceivePacketEvent(){
+//ReceivePacketEvent::ReceivePacketEvent(double _time):Event(_time){
 
-}
+//}
 
-ReceivePacketEvent::~ReceivePacketEvent(){
+//ReceivePacketEvent::~ReceivePacketEvent(){
 
-}
+//}
 
 // ------------------------------------------------------------------------- //
 ///////////////////////////////////////////////////////////////////////////////
 // EventManager
 ///////////////////////////////////////////////////////////////////////////////
 EventManager::EventManager(){
-
+    eventQ = new priority_queue<Event*, vector<Event*>, DereferenceCompareEvent >();
 }
 
-EventManager::~EventManager(){
-}
+EventManager::~EventManager(){/*{{{*/
+    clearItemsInQueue();
+    delete eventQ;
+}/*}}}*/
 
+//Public
+void EventManager::addEvent(Event* event)/*{{{*/
+{
+    eventQ->push(event);
+}/*}}}*/
+
+Event* EventManager::popEvent()/*{{{*/
+{
+    Event* poped = eventQ->top();
+    eventQ->pop();
+    return poped;
+}/*}}}*/
+
+void EventManager::clear()/*{{{*/
+{
+    clearItemsInQueue();
+    delete eventQ;
+    eventQ = new priority_queue<Event*, vector<Event*>, DereferenceCompareEvent >();
+}/*}}}*/
+
+int EventManager::size()/*{{{*/
+{
+    return eventQ->size();
+}/*}}}*/
+
+// private
+void EventManager::clearItemsInQueue()/*{{{*/
+{
+    while(!eventQ->empty())
+    {
+        Event* e = eventQ->top();
+        eventQ->pop();
+        delete e;
+    }
+}/*}}}*/
 
--- src/EventManager.hpp
+++ src/EventManager.hpp
@@ -1,7 +1,6 @@
 #ifndef __EVENT_MANAGER_HPP__
 #define __EVENT_MANAGER_HPP__
 
-//#include "boost/tuple/tuple.hpp"
 #include <string>
 #include <fstream>
 #include <iostream>
@@ -11,36 +10,63 @@
 #include <boost/foreach.hpp>
 #include <climits>
 #include <queue>
+#include <list>
+#include <functional>
 #define foreach BOOST_FOREACH
+
+using namespace std;
+
+typedef int Vertex;
+typedef int Content;
+
+// Comparator
 
 class Event{
     public:
-        Event();
+        Vertex fromUser;
+        Vertex toUser;
+        Content content;
+        //
+        Event(double _time);
         virtual ~Event();
+        //
+        double getEventTime() const;
+    private:
+        double time;
 };
 
 class ContentRequestedEvent: public Event{
-    public:
-        ContentRequestedEvent();
-        virtual ~ContentRequestedEvent();
 };
 
 class SendPacketEvent: public Event{
-    public:
-        SendPacketEvent();
-        virtual ~SendPacketEvent();
 };
 
 class ReceivePacketEvent: public Event{
-    public:
-        ReceivePacketEvent();
-        virtual ~ReceivePacketEvent();
 };
+
+
+struct DereferenceCompareEvent : public binary_function<Event*, Event*, bool>/*{{{*/
+{
+    bool operator()(const Event* lhs, const Event* rhs) const
+    {
+        return lhs->getEventTime() > rhs->getEventTime();
+    }
+};/*}}}*/
 
 class EventManager{
     public:
+        // Constructor/destructor
         EventManager();
         virtual ~EventManager();
+        // funcs
+        void addEvent(Event* event);
+        Event* popEvent();
+        void clear();
+        int size();
+    private:
+        priority_queue<Event*, vector<Event*>, DereferenceCompareEvent > *eventQ;
+        void clearItemsInQueue();
 };
+
 
 #endif

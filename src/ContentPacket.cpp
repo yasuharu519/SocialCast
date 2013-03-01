9d9f24893f5805fbb5038636d5ab1cc3d951c98c
Yasuharu Sawada <yasuharu519@gmail.com>
2013-03-02 00:15:00 +0900

    Add ContentPacket class

---
+++ src/ContentPacket.cpp
@@ -0,0 +1,20 @@
+#include "ContentPacket.hpp"
+
+// Content class
+Content::Content()
+{
+
+}
+
+Content::~Content()
+{
+}
+
+// Packet class
+Packet::Packet()
+{
+}
+
+Packet::~Packet()
+{
+}
---
+++ src/ContentPacket.hpp
@@ -0,0 +1,28 @@
+//
+//  Simulator.h
+//  SocialCast
+//
+//  Created by 澤田 泰治 on 2012/12/03.
+//  Copyright (c) 2012年 澤田 泰治. All rights reserved.
+//
+
+#ifndef __CONTENT_PACKET__
+#define __CONTENT_PACKET__
+
+class Content
+{
+    public:
+        Content();
+        virtual ~Content();
+};
+
+class Packet
+{
+    public:
+        Packet();
+        virtual ~Packet();
+};
+
+#endif 
+
+

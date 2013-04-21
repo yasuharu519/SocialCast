2df3ab5468ec711c7d2bd38d714e02bbf4dba68e
Yasuharu Sawada <yasuharu519@gmail.com>
2013-04-21 12:43:07 +0900

    add path analyzer

---
+++ src/PathAnalyze.py
@@ -0,0 +1,23 @@
+import sys
+
+table = dict()
+
+def resolve(path):
+    for i in xrange(len(path) - 1):
+        left, right = path[i], path[i+1]
+        if left > right:
+            left, right = right, left
+        if not table.has_key((left, right)):
+            table[(left, right)] = 1
+        else:
+            table[(left, right)] += 1
+
+for line in sys.stdin.readlines():
+    line = line.rstrip()
+    path = [int(x) for x in line.split(",")]
+    if len(path) > 1:
+        resolve(path)
+for key, value in sorted(table.items(),
+        key=lambda x:x[1],
+        reverse=True):
+    print value
